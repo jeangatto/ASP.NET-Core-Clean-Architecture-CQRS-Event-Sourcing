989abea03abafce4551d1110b2ae9966ec029306
Jean Gatto <jean_gatto@hotmail.com>
2022-06-04 21:47:16 -0300

    feat: adicionado comentários

--- src/Shop.Core/Interfaces/ICommand.cs
+++ src/Shop.Core/Interfaces/ICommand.cs
@@ -3,14 +3,14 @@ using MediatR;
 namespace Shop.Core.Interfaces;
 
 /// <summary>
-/// Interface marcadora para representar um comando com uma resposta vazia.
+/// Interface marcadora para representar um comando de escrita com uma resposta vazia.
 /// </summary>
 public interface ICommand : IRequest
 {
 }
 
 /// <summary>
-/// Interface marcadora para representar um comando com uma resposta.
+/// Interface marcadora para representar um comando de escrita com uma resposta.
 /// </summary>
 /// <typeparam name="TResponse">O tipo de resposta.</typeparam>
 public interface ICommand<out TResponse> : IRequest<TResponse>
--- src/Shop.Core/Interfaces/IQuery.cs
+++ src/Shop.Core/Interfaces/IQuery.cs
@@ -2,6 +2,10 @@ using MediatR;
 
 namespace Shop.Core.Interfaces;
 
+/// <summary>
+/// Interface marcadora para representar um comando de leitura com uma resposta.
+/// </summary>
+/// <typeparam name="TResponse">O tipo de resposta.</typeparam>
 public interface IQuery<out TResponse> : IRequest<TResponse>
 {
 }
