71c880923a916780e92dc5fb2a010bebc6241454
Jean Gatto <jean_gatto@hotmail.com>
2022-06-23 14:36:33 -0300

    feat: ajustes

--- src/Shop.Infrastructure/Behaviors/TransactionBehaviour.cs
+++ src/Shop.Infrastructure/Behaviors/TransactionBehaviour.cs
@@ -7,15 +7,15 @@ namespace Shop.Infrastructure.Behaviors;
 
 public class TransactionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
-    private readonly ITransaction _dbTransaction;
+    private readonly ITransaction _transaction;
 
-    public TransactionBehaviour(ITransaction dbTransaction) => _dbTransaction = dbTransaction;
+    public TransactionBehaviour(ITransaction transaction) => _transaction = transaction;
 
     public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
     {
         var response = default(TResponse);
 
-        await _dbTransaction.ExecuteAsync(async () => response = await next(), cancellationToken);
+        await _transaction.ExecuteAsync(async () => response = await next(), cancellationToken);
 
         return response;
     }
