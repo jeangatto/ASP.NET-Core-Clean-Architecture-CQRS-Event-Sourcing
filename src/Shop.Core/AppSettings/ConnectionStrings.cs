23cc8185519a55d210abcac29af464a70b16e0f2
Jean Gatto <jean_gatto@hotmail.com>
2022-07-24 11:59:58 -0300

    feat: refinamentos

--- src/Shop.Core/AppSettings/ConnectionStrings.cs
+++ src/Shop.Core/AppSettings/ConnectionStrings.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Core.AppSettings;
 
-public class ConnectionStrings
+public sealed class ConnectionStrings
 {
     [Required]
     public string ShopConnection { get; private set; }
