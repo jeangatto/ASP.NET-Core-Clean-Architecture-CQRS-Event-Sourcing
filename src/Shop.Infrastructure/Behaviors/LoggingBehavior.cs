422fc8c779f2f8364d1f9cf2e8c8bfcc887caa1a
Jean Gatto <jean_gatto@hotmail.com>
2022-10-24 09:52:22 -0300

    feat: refinamentos

--- src/Shop.Infrastructure/Behaviors/LoggingBehavior.cs
+++ src/Shop.Infrastructure/Behaviors/LoggingBehavior.cs
@@ -14,7 +14,7 @@ public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
 
     public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) => _logger = logger;
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var commandName = request.GetGenericTypeName();
 
