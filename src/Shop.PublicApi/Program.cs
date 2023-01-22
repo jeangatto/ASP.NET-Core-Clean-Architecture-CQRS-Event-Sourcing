319f73a09a5158295f27dba62f033a55a7e95c97
Jean Gatto <jean_gatto@hotmail.com>
2023-01-22 11:49:23 -0300

    feat: ajustes e refinamentos

--- src/Shop.PublicApi/Program.cs
+++ src/Shop.PublicApi/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,8 +22,9 @@ using Shop.PublicApi.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
-builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+builder.Services.Configure<KestrelServerOptions>(options => options.AddServerHeader = false);
 builder.Services.Configure<MvcNewtonsoftJsonOptions>(options => options.SerializerSettings.Configure());
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddResponseCompression(options => options.Providers.Add<GzipCompressionProvider>());
@@ -91,7 +93,7 @@ builder.Host.UseDefaultServiceProvider((context, options) =>
     options.ValidateOnBuild = true;
 });
 
-builder.WebHost.UseKestrel(options => options.AddServerHeader = false);
+builder.WebHost.UseKestrel();
 
 builder.Services.ConfigureAppSettings();
 builder.Services.AddInfrastructure();
