5fad5e6b38aa9a15b662e485a645e0135ae2acc6
Jean Gatto <jean_gatto@hotmail.com>
2023-06-21 09:32:27 -0300

    feat(domain): add error handling to CustomerFactory's Create method

--- src/Shop.Domain/Factories/CustomerFactory.cs
+++ src/Shop.Domain/Factories/CustomerFactory.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using Ardalis.Result;
 using Shop.Domain.Entities.CustomerAggregate;
 using Shop.Domain.ValueObjects;
 
@@ -6,8 +8,19 @@ namespace Shop.Domain.Factories;
 
 public static class CustomerFactory
 {
-    public static Customer Create(string firstName, string lastName, EGender gender, string email, DateTime dateOfBirth)
-        => new(firstName, lastName, gender, new Email(email), dateOfBirth);
+    public static Result<Customer> Create(
+        string firstName,
+        string lastName,
+        EGender gender,
+        string email,
+        DateTime dateOfBirth)
+    {
+        var emailResult = Email.Create(email);
+        if (!emailResult.IsSuccess)
+            return Result.Error(emailResult.Errors.ToArray());
+
+        return new Customer(firstName, lastName, gender, emailResult.Value, dateOfBirth);
+    }
 
     public static Customer Create(string firstName, string lastName, EGender gender, Email email, DateTime dateOfBirth)
         => new(firstName, lastName, gender, email, dateOfBirth);
