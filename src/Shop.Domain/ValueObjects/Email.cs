f503e2d0aefba4f77e18a09426aa2c70d38bd253
Jean Gatto <jean_gatto@hotmail.com>
2023-06-21 09:31:59 -0300

    feat: add email validation and creation to Email value object

--- src/Shop.Domain/ValueObjects/Email.cs
+++ src/Shop.Domain/ValueObjects/Email.cs
@@ -1,11 +1,26 @@
+using Ardalis.Result;
+using Shop.Core;
+
 namespace Shop.Domain.ValueObjects;
 
 public sealed record Email
 {
-    public Email(string address)
-        => Address = address.Trim().ToLowerInvariant();
+    private Email(string address)
+        => Address = address.ToLowerInvariant().Trim();
+
+    public Email() { } // Only for EF/ORM
 
     public string Address { get; }
+
+    public static Result<Email> Create(string emailAddress)
+    {
+        if (string.IsNullOrWhiteSpace(emailAddress))
+            return Result<Email>.Error("O endereço de e-mail deve ser informado.");
+
+        return !RegexPatterns.EmailIsValid.IsMatch(emailAddress)
+            ? Result<Email>.Error("O endereço de e-mail não é valido.")
+            : Result<Email>.Success(new Email(emailAddress));
+    }
 
     public override string ToString()
         => Address;
