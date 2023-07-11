91d752798ebc824f801ffae02ce6e0c16891055d
Jean Gatto <jean_gatto@hotmail.com>
2023-07-11 08:37:58 -0300

    chore(RegexPatterns.cs): update comments for EmailIsValid regex pattern

--- src/Shop.Core/RegexPatterns.cs
+++ src/Shop.Core/RegexPatterns.cs
@@ -4,10 +4,11 @@ namespace Shop.Core;
 
 public static partial class RegexPatterns
 {
-    // Regular expression pattern to validate email addresses
+    /// <summary>
+    /// Regular expression pattern to validate email addresses
+    /// </summary>
     public static readonly Regex EmailIsValid = EmailRegexPatternAttr();
 
-    // Generate the regular expression pattern for email validation
     [GeneratedRegex(
         "^([0-9a-zA-Z]([\\+\\-_\\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]*\\.)+[a-zA-Z0-9]{2,17})$",
         RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant)]
