128fd304f2d8f4ecd0bc9d6cc7d6cdc5742b2bc5
Jean Gatto <jean_gatto@hotmail.com>
2023-08-01 08:30:33 -0300

    refactor(WriteDbContext.cs): change variable names from _encryptionIv and _encryptionKey to EncryptionIv and EncryptionKey

--- src/Shop.Infrastructure/Data/Context/WriteDbContext.cs
+++ src/Shop.Infrastructure/Data/Context/WriteDbContext.cs
@@ -8,25 +8,24 @@ namespace Shop.Infrastructure.Data.Context;
 
 public class WriteDbContext : BaseDbContext<WriteDbContext>
 {
-    private readonly byte[] _encryptionIv = { 163, 225, 4, 33, 227, 178, 113, 128, 174, 23, 9, 144, 213, 158, 134, 48 };
-
-    private readonly byte[] _encryptionKey =
-    {
-        189, 3, 80, 118, 242, 164, 9, 197, 106, 166, 122, 118, 161, 212, 106, 26, 171, 18, 178, 98, 86, 102, 196, 6,
-        78, 249, 4, 164, 66, 154, 218, 126
-    };
+    private static readonly byte[] EncryptionIv = { 163, 225, 4, 33, 227, 178, 113, 128, 174, 23, 9, 144, 213, 158, 134, 48 };
+    private static readonly byte[] EncryptionKey = { 189, 3, 80, 118, 242, 164, 9, 197, 106, 166, 122, 118, 161, 212, 106, 26, 171, 18, 178, 98, 86, 102, 196, 6, 78, 249, 4, 164, 66, 154, 218, 126 };
 
     private readonly IEncryptionProvider _encryptionProvider;
 
     public WriteDbContext(DbContextOptions<WriteDbContext> dbOptions) : base(dbOptions) =>
-        _encryptionProvider = new AesProvider(_encryptionKey, _encryptionIv);
+        _encryptionProvider = new AesProvider(EncryptionKey, EncryptionIv);
 
     internal DbSet<Customer> Customers => Set<Customer>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
         modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+
+        // Data Encryption
+        // https://github.com/Eastrall/EntityFrameworkCore.DataEncryption
         modelBuilder.UseEncryption(_encryptionProvider);
     }
 }
