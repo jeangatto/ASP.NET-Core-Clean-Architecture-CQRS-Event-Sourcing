fdee5e43d569d0b7b942e3bdb9f24c393193b693
Jean Gatto <jean_gatto@hotmail.com>
2023-08-03 09:19:54 -0300

    refactor(RegexPatterns.cs): simplify EmailIsValid regex pattern initialization

--- src/Shop.Core/RegexPatterns.cs
+++ src/Shop.Core/RegexPatterns.cs
@@ -2,15 +2,12 @@ using System.Text.RegularExpressions;
 
 namespace Shop.Core;
 
-public static partial class RegexPatterns
+public static class RegexPatterns
 {
     /// <summary>
     /// Regular expression pattern to validate email addresses
     /// </summary>
-    public static readonly Regex EmailIsValid = EmailRegexPatternAttr();
-
-    [GeneratedRegex(
-        "^([0-9a-zA-Z]([\\+\\-_\\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]*\\.)+[a-zA-Z0-9]{2,17})$",
-        RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant)]
-    private static partial Regex EmailRegexPatternAttr();
+    public static readonly Regex EmailIsValid =
+        new("^([0-9a-zA-Z]([\\+\\-_\\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]*\\.)+[a-zA-Z0-9]{2,17})$",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant);
 }
