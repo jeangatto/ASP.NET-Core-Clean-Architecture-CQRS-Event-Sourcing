a79d2165e80e332c884ef6c4741894f54ba94803
Jean Gatto <jean_gatto@hotmail.com>
2023-08-03 13:51:37 -0300

    chore(RegexPatterns.cs): refactor EmailIsValid regex pattern to use generated regex attribute

--- src/Shop.Core/RegexPatterns.cs
+++ src/Shop.Core/RegexPatterns.cs
@@ -2,12 +2,15 @@ using System.Text.RegularExpressions;
 
 namespace Shop.Core;
 
-public static class RegexPatterns
+public static partial class RegexPatterns
 {
     /// <summary>
     /// Regular expression pattern to validate email addresses
     /// </summary>
-    public static readonly Regex EmailIsValid =
-        new("^([0-9a-zA-Z]([\\+\\-_\\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]*\\.)+[a-zA-Z0-9]{2,17})$",
-            RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant);
+    public static readonly Regex EmailIsValid = EmailRegexPatternAttr();
+
+    [GeneratedRegex(
+        "^([0-9a-zA-Z]([\\+\\-_\\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]*\\.)+[a-zA-Z0-9]{2,17})$",
+        RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant)]
+    private static partial Regex EmailRegexPatternAttr();
 }
