546e4be9cd77652ab00cb0e7f1892ac23edc65cd
Jean Gatto <jean_gatto@hotmail.com>
2023-08-05 11:52:20 -0300

    feat(JsonExtensions.cs): add method documentation

--- src/Shop.Core/Extensions/JsonExtensions.cs
+++ src/Shop.Core/Extensions/JsonExtensions.cs
@@ -29,10 +29,10 @@ public static class JsonExtensions
         value != null ? JsonSerializer.Serialize(value, JsonOptions) : default;
 
     /// <summary>
-    /// Configures the JsonSerializerSettings instance.
+    /// Configures the JsonSerializerOptions instance.
     /// </summary>
-    /// <param name="jsonSettings">The JsonSerializerSettings instance to configure.</param>
-    /// <returns>The configured JsonSerializerSettings instance.</returns>
+    /// <param name="jsonSettings">The JsonSerializerOptions instance to configure.</param>
+    /// <returns>The configured JsonSerializerOptions instance.</returns>
     public static JsonSerializerOptions Configure(this JsonSerializerOptions jsonSettings)
     {
         jsonSettings.WriteIndented = false;
@@ -47,13 +47,22 @@ public static class JsonExtensions
 
 internal sealed class PrivateConstructorContractResolver : DefaultJsonTypeInfoResolver
 {
+    /// <summary>
+    /// Gets the JSON type information for the specified type, with support for creating objects with private constructors.
+    /// </summary>
+    /// <param name="type">The type to get the JSON type information for.</param>
+    /// <param name="options">The <see cref="JsonSerializerOptions"/> to use for serialization.</param>
+    /// <returns>The JSON type information for the specified type.</returns>
     public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
     {
         var jsonTypeInfo = base.GetTypeInfo(type, options);
+
+        // Check if the type is an object, has no public constructor, and CreateObject is not already set
         if (jsonTypeInfo.Kind == JsonTypeInfoKind.Object
             && jsonTypeInfo.CreateObject is null
             && jsonTypeInfo.Type.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Length == 0)
         {
+            // Set CreateObject to a lambda expression that creates an instance using a private constructor
             jsonTypeInfo.CreateObject = () => Activator.CreateInstance(jsonTypeInfo.Type, true);
         }
 
