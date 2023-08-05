8af7bbfa6f449a7680696ff4f70936b4ae6eb45b
Jean Gatto <jean_gatto@hotmail.com>
2023-08-05 11:46:30 -0300

    refactor(JsonExtensions.cs): Update JsonExtensions class to use System.Text.Json instead of Newtonsoft.Json

--- src/Shop.Core/Extensions/JsonExtensions.cs
+++ src/Shop.Core/Extensions/JsonExtensions.cs
@@ -1,16 +1,14 @@
-using JsonNet.ContractResolvers;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
+using System;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 
 namespace Shop.Core.Extensions;
 
 public static class JsonExtensions
 {
-    private static readonly CamelCaseNamingStrategy CamelCaseNaming = new();
-    private static readonly StringEnumConverter StringEnumConverter = new(CamelCaseNaming);
-    private static readonly PrivateSetterContractResolver ContractResolver = new() { NamingStrategy = CamelCaseNaming };
-    private static readonly JsonSerializerSettings DefaultJsonSettings = new JsonSerializerSettings().Configure();
+    private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions().Configure();
 
     /// <summary>
     /// Converts a JSON string to an object of type T.
@@ -19,7 +17,7 @@ public static class JsonExtensions
     /// <param name="value">The JSON string to deserialize.</param>
     /// <returns>The deserialized object of type T.</returns>
     public static T FromJson<T>(this string value) =>
-        value != null ? JsonConvert.DeserializeObject<T>(value, DefaultJsonSettings) : default;
+        value != null ? JsonSerializer.Deserialize<T>(value, JsonOptions) : default;
 
     /// <summary>
     /// Converts an object to JSON string.
@@ -28,27 +26,37 @@ public static class JsonExtensions
     /// <param name="value">The object to convert.</param>
     /// <returns>The JSON string representation of the object.</returns>
     public static string ToJson<T>(this T value) =>
-        value != null ? JsonConvert.SerializeObject(value, typeof(T), DefaultJsonSettings) : default;
+        value != null ? JsonSerializer.Serialize(value, JsonOptions) : default;
 
     /// <summary>
     /// Configures the JsonSerializerSettings instance.
     /// </summary>
     /// <param name="jsonSettings">The JsonSerializerSettings instance to configure.</param>
     /// <returns>The configured JsonSerializerSettings instance.</returns>
-    public static JsonSerializerSettings Configure(this JsonSerializerSettings jsonSettings)
+    public static JsonSerializerOptions Configure(this JsonSerializerOptions jsonSettings)
     {
-        // Ignore circular references
-        jsonSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-        // Disable preserving references
-        jsonSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
-        // Ignore null values during serialization
-        jsonSettings.NullValueHandling = NullValueHandling.Ignore;
-        // Disable formatting of the JSON output
-        jsonSettings.Formatting = Formatting.None;
-        // Set the contract resolver for custom serialization behavior
-        jsonSettings.ContractResolver = ContractResolver;
-        // Add a converter for string enum values
-        jsonSettings.Converters.Add(StringEnumConverter);
+        jsonSettings.WriteIndented = false;
+        jsonSettings.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        jsonSettings.ReadCommentHandling = JsonCommentHandling.Skip;
+        jsonSettings.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        jsonSettings.TypeInfoResolver = new PrivateConstructorContractResolver();
+        jsonSettings.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
         return jsonSettings;
+    }
+}
+
+internal sealed class PrivateConstructorContractResolver : DefaultJsonTypeInfoResolver
+{
+    public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+        var jsonTypeInfo = base.GetTypeInfo(type, options);
+        if (jsonTypeInfo.Kind == JsonTypeInfoKind.Object
+            && jsonTypeInfo.CreateObject is null
+            && jsonTypeInfo.Type.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Length == 0)
+        {
+            jsonTypeInfo.CreateObject = () => Activator.CreateInstance(jsonTypeInfo.Type, true);
+        }
+
+        return jsonTypeInfo;
     }
 }
