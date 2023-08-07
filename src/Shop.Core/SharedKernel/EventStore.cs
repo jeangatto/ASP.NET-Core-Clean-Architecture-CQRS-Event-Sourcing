db7b032e2b35667571b00fe58e44b3f162bd8ea5
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:20:18 -0300

    chore(EventStore.cs): add comments and improve code readability

--- src/Shop.Core/SharedKernel/EventStore.cs
+++ src/Shop.Core/SharedKernel/EventStore.cs
@@ -2,8 +2,17 @@ using System;
 
 namespace Shop.Core.SharedKernel;
 
+/// <summary>
+/// Represents the event store for storing events.
+/// </summary>
 public class EventStore : BaseEvent
 {
+    /// <summary>
+    /// Initializes a new instance of the EventStore class.
+    /// </summary>
+    /// <param name="aggregateId">The aggregate ID.</param>
+    /// <param name="messageType">The message type.</param>
+    /// <param name="data">The data.</param>
     public EventStore(Guid aggregateId, string messageType, string data)
     {
         AggregateId = aggregateId;
@@ -11,8 +20,20 @@ public class EventStore : BaseEvent
         Data = data;
     }
 
-    public EventStore() { } // Default constructor for EF/ORM
+    /// <summary>
+    /// Default constructor for Entity Framework or other ORM frameworks.
+    /// </summary>
+    public EventStore()
+    {
+    }
 
+    /// <summary>
+    /// Gets or sets the ID.
+    /// </summary>
     public Guid Id { get; private init; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Gets or sets the data.
+    /// </summary>
     public string Data { get; private init; }
 }
