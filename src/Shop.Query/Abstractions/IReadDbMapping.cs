1aff777cd683724f0576b405808e057bd38ecf23
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:13:46 -0300

    feat(Shop.Query): add IReadDbMapping interface for reading database mappings docs(Shop.Query): add summary and method documentation for IReadDbMapping interface

--- src/Shop.Query/Abstractions/IReadDbMapping.cs
+++ src/Shop.Query/Abstractions/IReadDbMapping.cs
@@ -1,6 +1,12 @@
 namespace Shop.Query.Abstractions;
 
+/// <summary>
+/// Represents an interface for reading database mappings.
+/// </summary>
 public interface IReadDbMapping
 {
+    /// <summary>
+    /// Configures the mappings for reading from the database.
+    /// </summary>
     void Configure();
 }
