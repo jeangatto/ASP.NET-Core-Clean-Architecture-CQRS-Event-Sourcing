b5b1105f22239640e59aeaa94e4ce2db1cfe0383
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:10:30 -0300

    chore(BaseQueryModel.cs): delete BaseQueryModel.cs file

--- src/Shop.Query/QueriesModel/BaseQueryModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Shop.Query.Abstractions;
-
-namespace Shop.Query.QueriesModel;
-
-public abstract class BaseQueryModel : IQueryModel<Guid>
-{
-    public Guid Id { get; } = Guid.NewGuid();
-}
