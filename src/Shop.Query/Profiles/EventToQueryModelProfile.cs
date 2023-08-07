64d8826e65e158f521a3ca9ca578b9f631fe8efe
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:09:46 -0300

    refactor(EventToQueryModelProfile.cs): refactor mapping configuration for Customer events in EventToQueryModelProfile

--- src/Shop.Query/Profiles/EventToQueryModelProfile.cs
+++ src/Shop.Query/Profiles/EventToQueryModelProfile.cs
@@ -8,8 +8,16 @@ public class EventToQueryModelProfile : Profile
 {
     public EventToQueryModelProfile()
     {
-        CreateMap<CustomerCreatedEvent, CustomerQueryModel>(MemberList.Destination);
-        CreateMap<CustomerUpdatedEvent, CustomerQueryModel>(MemberList.Destination);
-        CreateMap<CustomerDeletedEvent, CustomerQueryModel>(MemberList.Destination);
+        CreateMap<CustomerCreatedEvent, CustomerQueryModel>(MemberList.Destination)
+            .ConstructUsing(@event => Create(@event));
+
+        CreateMap<CustomerUpdatedEvent, CustomerQueryModel>(MemberList.Destination)
+            .ConstructUsing(@event => Create(@event));
+
+        CreateMap<CustomerDeletedEvent, CustomerQueryModel>(MemberList.Destination)
+           .ConstructUsing(@event => Create(@event));
     }
+
+    private static CustomerQueryModel Create(CustomerBaseEvent @event) =>
+        new(@event.Id, @event.FirstName, @event.LastName, @event.Gender.ToString(), @event.Email, @event.DateOfBirth);
 }
