1d2bb17b5b872c5e95fc21afd01b95ac50f1c799
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:13:38 -0300

    chore(IQueryModel.cs): add XML documentation for IQueryModel interface

--- src/Shop.Query/Abstractions/IQueryModel.cs
+++ src/Shop.Query/Abstractions/IQueryModel.cs
@@ -2,11 +2,21 @@ using System;
 
 namespace Shop.Query.Abstractions;
 
+/// <summary>
+/// Represents the query model interface.
+/// </summary>
 public interface IQueryModel
 {
 }
 
+/// <summary>
+/// Represents the interface for a query model with a generic key type.
+/// </summary>
+/// <typeparam name="TKey">The type of the key.</typeparam>
 public interface IQueryModel<out TKey> : IQueryModel where TKey : IEquatable<TKey>
 {
+    /// <summary>
+    /// Gets the ID of the query model.
+    /// </summary>
     TKey Id { get; }
 }
