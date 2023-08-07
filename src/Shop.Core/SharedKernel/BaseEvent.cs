f33144fe01a13a258527ea91f63ae145cd8b67b2
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:20:10 -0300

    chore(BaseEvent.cs): add comments and improve code readability

--- src/Shop.Core/SharedKernel/BaseEvent.cs
+++ src/Shop.Core/SharedKernel/BaseEvent.cs
@@ -3,9 +3,23 @@ using MediatR;
 
 namespace Shop.Core.SharedKernel;
 
+/// <summary>
+/// Represents a base event.
+/// </summary>
 public abstract class BaseEvent : INotification
 {
+    /// <summary>
+    /// Gets the type of the message.
+    /// </summary>
     public string MessageType { get; protected init; }
+
+    /// <summary>
+    /// Gets the aggregate ID.
+    /// </summary>
     public Guid AggregateId { get; protected init; }
+
+    /// <summary>
+    /// Gets the date and time when the event occurred.
+    /// </summary>
     public DateTime OccurredOn { get; private init; } = DateTime.Now;
 }
