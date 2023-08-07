c5981294f316775020560b76389baf8f849ff25d
Jean Gatto <jean_gatto@hotmail.com>
2023-08-07 11:20:27 -0300

    chore(IWriteOnlyRepository.cs): add XML documentation comments to interface and methods

--- src/Shop.Core/SharedKernel/IWriteOnlyRepository.cs
+++ src/Shop.Core/SharedKernel/IWriteOnlyRepository.cs
@@ -3,10 +3,34 @@ using System.Threading.Tasks;
 
 namespace Shop.Core.SharedKernel;
 
+/// <summary>
+/// Represents a repository that allows write-only operations on entities.
+/// </summary>
+/// <typeparam name="TEntity">The type of entity.</typeparam>
 public interface IWriteOnlyRepository<TEntity> : IDisposable where TEntity : IEntity<Guid>
 {
+    /// <summary>
+    /// Adds a new entity to the repository.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
     void Add(TEntity entity);
+
+    /// <summary>
+    /// Updates an existing entity in the repository.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
     void Update(TEntity entity);
+
+    /// <summary>
+    /// Removes an entity from the repository.
+    /// </summary>
+    /// <param name="entity">The entity to remove.</param>
     void Remove(TEntity entity);
+
+    /// <summary>
+    /// Retrieves an entity by its ID asynchronously.
+    /// </summary>
+    /// <param name="id">The ID of the entity to retrieve.</param>
+    /// <returns>The retrieved entity.</returns>
     Task<TEntity> GetByIdAsync(Guid id);
 }
