96cd4209dc664c2dd7a61b4a14a4fa152f47f335
Jean Gatto <jean_gatto@hotmail.com>
2023-08-13 10:51:56 -0300

    refactor(IReadDbContext.cs): Remove unused methods and update comments

--- src/Shop.Query/Abstractions/IReadDbContext.cs
+++ src/Shop.Query/Abstractions/IReadDbContext.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 
 namespace Shop.Query.Abstractions;
 
+/// <summary>
+/// Represents the read-only database context for querying data.
+/// </summary>
 public interface IReadDbContext
 {
     /// <summary>
@@ -17,27 +18,7 @@ public interface IReadDbContext
     /// </summary>
     /// <typeparam name="TQueryModel">The type of the query model.</typeparam>
     /// <returns>The MongoDB collection for the specified query model.</returns>
-    IMongoCollection<TQueryModel> GetCollection<TQueryModel>()
-        where TQueryModel : IQueryModel;
-
-    /// <summary>
-    /// Upserts a query model into the database.
-    /// </summary>
-    /// <typeparam name="TQueryModel">The type of the query model.</typeparam>
-    /// <param name="queryModel">The query model to upsert.</param>
-    /// <param name="upsertFilter">The filter expression to determine the upsert condition.</param>
-    /// <returns>A task representing the asynchronous upsert operation.</returns>
-    Task UpsertAsync<TQueryModel>(TQueryModel queryModel, Expression<Func<TQueryModel, bool>> upsertFilter)
-        where TQueryModel : IQueryModel;
-
-    /// <summary>
-    /// Deletes query models from the database that match the specified filter.
-    /// </summary>
-    /// <typeparam name="TQueryModel">The type of the query model.</typeparam>
-    /// <param name="deleteFilter">The filter expression to determine which query models to delete.</param>
-    /// <returns>A task representing the asynchronous delete operation.</returns>
-    Task DeleteAsync<TQueryModel>(Expression<Func<TQueryModel, bool>> deleteFilter)
-        where TQueryModel : IQueryModel;
+    IMongoCollection<TQueryModel> GetCollection<TQueryModel>() where TQueryModel : IQueryModel;
 
     /// <summary>
     /// Creates collections in the database for all query models.
