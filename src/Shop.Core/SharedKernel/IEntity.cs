0288490a33c130b2476f4e49efa0d398abd4537f
Jean Gatto <jean_gatto@hotmail.com>
2023-08-16 17:09:46 -0300

    docs(IEntity.cs): add base interface for all entities with a key

--- src/Shop.Core/SharedKernel/IEntity.cs
+++ src/Shop.Core/SharedKernel/IEntity.cs
@@ -9,6 +9,10 @@ public interface IEntity
 {
 }
 
+/// <summary>
+/// This is the base interface for all entities with a key.
+/// </summary>
+/// <typeparam name="TKey">The type of the entity key.</typeparam>
 public interface IEntity<out TKey> : IEntity where TKey : IEquatable<TKey>
 {
     /// <summary>
