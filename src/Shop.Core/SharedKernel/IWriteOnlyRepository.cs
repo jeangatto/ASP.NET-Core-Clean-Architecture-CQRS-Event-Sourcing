e8f568ec171b9e391babdd7344c36ad5da0aebb2
Jean Gatto <jean_gatto@hotmail.com>
2023-08-18 10:53:02 -0300

    chore(sharedkernel): update IWriteOnlyRepository interface signature

--- src/Shop.Core/SharedKernel/IWriteOnlyRepository.cs
+++ src/Shop.Core/SharedKernel/IWriteOnlyRepository.cs
@@ -8,7 +8,7 @@ namespace Shop.Core.SharedKernel;
 /// </summary>
 /// <typeparam name="TEntity">The type of entity.</typeparam>
 /// <typeparam name="TKey">The type of the entity's key.</typeparam>
-public interface IWriteOnlyRepository<TEntity, TKey> : IDisposable
+public interface IWriteOnlyRepository<TEntity, in TKey> : IDisposable
     where TEntity : IEntity<TKey>
     where TKey : IEquatable<TKey>
 {
