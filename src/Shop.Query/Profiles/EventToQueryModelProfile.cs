5e55bc847c4cfba10d2f330a033e131b458fbdc0
Jean Gatto <jean_gatto@hotmail.com>
2023-08-26 08:22:28 -0300

    refactor(EventToQueryModelProfile.cs): rename Create method to CreateCustomerQueryModel for clarity and consistency

--- src/Shop.Query/Profiles/EventToQueryModelProfile.cs
+++ src/Shop.Query/Profiles/EventToQueryModelProfile.cs
@@ -9,17 +9,17 @@ public class EventToQueryModelProfile : Profile
     public EventToQueryModelProfile()
     {
         CreateMap<CustomerCreatedEvent, CustomerQueryModel>(MemberList.Destination)
-            .ConstructUsing(@event => Create(@event));
+            .ConstructUsing(@event => CreateCustomerQueryModel(@event));
 
         CreateMap<CustomerUpdatedEvent, CustomerQueryModel>(MemberList.Destination)
-            .ConstructUsing(@event => Create(@event));
+            .ConstructUsing(@event => CreateCustomerQueryModel(@event));
 
         CreateMap<CustomerDeletedEvent, CustomerQueryModel>(MemberList.Destination)
-            .ConstructUsing(@event => Create(@event));
+            .ConstructUsing(@event => CreateCustomerQueryModel(@event));
     }
 
     public override string ProfileName => nameof(EventToQueryModelProfile);
 
-    private static CustomerQueryModel Create<TEvent>(TEvent @event) where TEvent : CustomerBaseEvent =>
+    private static CustomerQueryModel CreateCustomerQueryModel<TEvent>(TEvent @event) where TEvent : CustomerBaseEvent =>
         new(@event.Id, @event.FirstName, @event.LastName, @event.Gender.ToString(), @event.Email, @event.DateOfBirth);
 }
