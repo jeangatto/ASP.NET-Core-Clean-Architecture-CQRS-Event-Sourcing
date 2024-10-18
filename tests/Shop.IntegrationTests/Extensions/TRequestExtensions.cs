88e9cd4a10cbc36465d3575251632d891cbcafa7
Jean Gatto <jean_gatto@hotmail.com>
2024-10-18 07:58:13 -0300

    chore(Directory.Packages.props): update nuget packages
    Signed-off-by: Jean Gatto <jean_gatto@hotmail.com>

--- tests/Shop.IntegrationTests/Extensions/TRequestExtensions.cs
+++ tests/Shop.IntegrationTests/Extensions/TRequestExtensions.cs
@@ -5,7 +5,7 @@ using Shop.Core.Extensions;
 
 namespace Shop.IntegrationTests.Extensions;
 
-public static class TExtensions
+public static class TRequestExtensions
 {
     public static HttpContent ToJsonHttpContent<TRequest>(this TRequest request) =>
         new StringContent(request.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
