d1273196d1bb78b375106d57f64128fabc67c58f
Jean Gatto <jean_gatto@hotmail.com>
2024-12-22 09:37:39 -0300

    Improve MongoDB health checks in AddHealthChecks method

--- src/Shop.PublicApi/Extensions/ServicesCollectionExtensions.cs
+++ src/Shop.PublicApi/Extensions/ServicesCollectionExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Shop.Core.AppSettings;
 using Shop.Core.Extensions;
 using Shop.Infrastructure;
@@ -33,7 +34,7 @@ internal static class ServicesCollectionExtensions
             .AddHealthChecks()
             .AddDbContextCheck<WriteDbContext>(tags: DbRelationalTags)
             .AddDbContextCheck<EventStoreDbContext>(tags: DbRelationalTags)
-            .AddMongoDb(options.NoSqlConnection, tags: DbNoSqlTags);
+            .AddMongoDb(clientFactory: _ => new MongoClient(options.NoSqlConnection), tags: DbNoSqlTags);
 
         if (!options.CacheConnectionInMemory())
             healthCheckBuilder.AddRedis(options.CacheConnection);
