455acb6367263c7daa8813d2ff65cf61a4624e7c
Jean Gatto <jean_gatto@hotmail.com>
2025-04-05 08:35:23 -0300

    refactor(EGender): remove Description attributes from enum values

--- src/Shop.Domain/Entities/CustomerAggregate/EGender.cs
+++ src/Shop.Domain/Entities/CustomerAggregate/EGender.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System.Text.Json.Serialization;
 
 namespace Shop.Domain.Entities.CustomerAggregate;
@@ -6,12 +5,7 @@ namespace Shop.Domain.Entities.CustomerAggregate;
 [JsonConverter(typeof(JsonStringEnumConverter<EGender>))]
 public enum EGender
 {
-    [Description("Não informar")]
     None = 0,
-
-    [Description("Masculino")]
     Male = 1,
-
-    [Description("Feminino")]
     Female = 2
 }
