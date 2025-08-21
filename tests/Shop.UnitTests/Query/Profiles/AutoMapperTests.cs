28c8a9712b8bad6ab974bb4da48785cf8eceb1ea
Jean Gatto <jean_gatto@hotmail.com>
2025-08-21 11:18:25 -0300

    refactor(AutoMapperTests): add ILoggerFactory to Mapper configuration for improved logging

--- tests/Shop.UnitTests/Query/Profiles/AutoMapperTests.cs
+++ tests/Shop.UnitTests/Query/Profiles/AutoMapperTests.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Shop.Query.Profiles;
 using Xunit;
 using Xunit.Categories;
@@ -13,7 +14,9 @@ public class AutoMapperTests
     public void Should_Mapper_ConfigurationIsValid()
     {
         // Arrange
-        var mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<EventToQueryModelProfile>()));
+        var loggerFactory = NSubstitute.Substitute.For<ILoggerFactory>();
+
+        var mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<EventToQueryModelProfile>(), loggerFactory));
 
         // Act
         // REF: https://fluentassertions.com/exceptions/
